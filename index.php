5737d9ff74dc03d5db53dc9b7bef3d906d59c4a2
elpaulo <paulfournier64@gmail.com>
2015-04-18 15:01:21 +0200

    Physics tests

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>LD32</title>
+		<script type="text/javascript" src="phaser.js"></script>
+		<script type="text/javascript" src="game.js"></script>
+        <style>
+            body {
+                background-color: #7192b7;
+            }
+        </style>
+	</head>
+	<body>
+    <center>
+		<div id="phaser-example" />
+    </center>
+	</body>
+</html>
